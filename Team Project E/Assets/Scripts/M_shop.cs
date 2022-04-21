1c85cb9d618697b5a79fc63ebac55eace56143e1
magamelon <103184072+magamelon@users.noreply.github.com>
2022-04-21 21:01:14 +0900

    일단 상점UI 스크립트와 프리팹만 넣었습니다

---
+++ Team Project E/Assets/Scripts/M_shop.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class M_shop : MonoBehaviour
+{
+
+    public GameObject layout;
+    public event UnityAction shopOpen;
+    public event UnityAction Open;
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void BuyManyOpen()
+    {
+        GameObject obj = Instantiate(Resources.Load("UI/M_Buymany"), this.transform) as GameObject;
+        obj.transform.position = new Vector3(Screen.width / 2, Screen.height / 2, 0.0f);
+        this.GetComponentInChildren<M_Buymany>().layout += () => layout.SetActive(false);
+        layout.SetActive(true);       
+    }
+    public void Close()
+    {
+        shopOpen.Invoke();
+        Open.Invoke();
+        Destroy(this.gameObject);
+    }
+}
