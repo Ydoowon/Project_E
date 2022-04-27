b0d7da8a97d62a354758d02f77deb5b29924a835
magamelon <103184072+magamelon@users.noreply.github.com>
2022-04-27 21:56:46 +0900

    상점UI일단은 추가했습니다

--- Team Project E/Assets/Scripts/M_ShopOpen.cs
+++ Team Project E/Assets/Scripts/M_ShopOpen.cs
@@ -5,9 +5,10 @@ using UnityEngine.Events;
 
 public class M_ShopOpen : MonoBehaviour
 {
-    public event UnityAction Shoping = null;
-    public TMPro.TMP_Text How;
-    public GameObject HOW;
+    public event UnityAction Shoprng = null;
+    public TMPro.TMP_Text How; // 설명문 글씨
+    public GameObject HOW; // 설명문 자체
+    public GameObject UI; // 캔버스에 있는 UI스크립트
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +20,22 @@ public class M_ShopOpen : MonoBehaviour
     {
         
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (UI.GetComponent<M_ui>().Shopshow == true) HOW.SetActive(true);
+        else HOW.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        HOW.SetActive(true);
         How.text = "<#ff0000ff>E</color> <#000000ff>키를 눌러 상점열기";
-        Shoping?.Invoke();
-
+        Shoprng?.Invoke();
     }
+
     private void OnTriggerExit(Collider other)
     {
         HOW.SetActive(false);
-        Shoping?.Invoke();
-        GameObject.Find("Canvas").GetComponentInChildren<M_shop>()?.Close();
+        Shoprng?.Invoke();
+        UI.GetComponentInChildren<M_shop>()?.Close();
 
 
     }
--- Team Project E/Assets/Scripts/M_ui.cs
+++ Team Project E/Assets/Scripts/M_ui.cs
@@ -7,14 +7,15 @@ public class M_ui : MonoBehaviour
 {
     bool Menushow = true; // esc키를 눌러서 나온 메뉴창이 나왔는지 안나왔는지
     bool Shoprng = false; // Shop을 열 범위에 있는지 없는지
-    bool Shopshow = true; // Shop이 열려 있는지 안열려있는지
+   public bool Shopshow = true; // Shop이 열려 있는지 안열려있는지
 
-
+    public GameObject ShopRnge;
     // Start is called before the first frame update
     void Start()
     {
 
-        GameObject.Find("Shoprnge").GetComponent<M_ShopOpen>().Shoping += () => Shoprng = !Shoprng;            
+        ShopRnge.GetComponent<M_ShopOpen>().Shoprng += () => Shoprng = !Shoprng;
+        
      
     }
 
