5b48f54de3f0767ae240676a27988f5073f9cec7
Magamelon <rlarb_gus@naver.com>
2022-05-20 19:30:46 +0900

    일단 바꿈

--- Team Project E/Assets/Scripts/M_playerShopOpen.cs
+++ Team Project E/Assets/Scripts/M_playerShopOpen.cs
@@ -7,32 +7,45 @@ public class M_playerShopOpen : MonoBehaviour
 {
     public TMPro.TMP_Text How;
     public GameObject HOW;
-    public event UnityAction PlayerShoprng;
     public GameObject Shop;
+    bool PlayerShopshow = false;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Shop.GetComponent<M_PlayerShop>().Shophow += () =>
+        {
+            PlayerShopshow = false;
+            HOW.SetActive(true);
 
-    // Update is called once per frame
-    void Update()
-    {
-   
-
+        };
     }
     private void OnTriggerEnter(Collider other)
     {
-        PlayerShoprng?.Invoke();
         HOW.SetActive(true);
         How.text = "진열대 열기";
 
     }
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            if (!PlayerShopshow)
+            {
+                Shop.SetActive(true);
+                PlayerShopshow = true;
+                HOW.SetActive(false);
+            }
+            else
+            {
+                Shop.GetComponent<M_PlayerShop>().Close();
+            }
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
-        PlayerShoprng?.Invoke();
-        HOW.SetActive(false);
         Shop.GetComponent<M_PlayerShop>().Close();
-
+        HOW.SetActive(false);
     }
 }
